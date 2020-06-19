b9dc0fe14da0c00de46fb64fc2769077e6b52177
grety1016 <snl1016@163.com>
2020-06-19 09:03:57 +0800

    1

--- WorkingWithFileSystems/Program.cs
+++ WorkingWithFileSystems/Program.cs
@@ -41,12 +41,31 @@ namespace WorkingWithFileSystems
                 }
             }
         }
+        static void WorkWithDirectories()
+        {
+            // define a directory path for a new folder
+            // starting in the user' s folder
+            var newFolder = Combine(GetFolderPath(SpecialFolder.Personal),"Code","Chapter09","NewFolder");
+            WriteLine($"Working with: {newFolder}") ;
+            // check if it exists
+            WriteLine($"Does it exist? {Exists(newFolder) }") ;
+            // create directory
+            WriteLine("Creating it. . . ") ;
+            CreateDirectory(newFolder) ;
+            WriteLine($"Does it exist? {Exists(newFolder) }") ;
+            Write("Confirm the directory exists, and then press ENTER: ") ;
+            ReadLine() ;
+            // delete directory
+            WriteLine("Deleting it. . . ") ;
+            Delete(newFolder,recursive:true) ;
+            WriteLine($"Does it exist? {Exists(newFolder) }");
+        }
         static void Main(string[] args)
         {
-            Program.OutputFileSystemInfo();
+            //Program.OutputFileSystemInfo();
+            //WorkWithDrives(); 
+            WorkWithDirectories();
 
-            WorkWithDrives(); 
-                       
         }
     }
 }
